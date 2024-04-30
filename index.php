c1b10300c8c137f8deb0c2ecc864248bc531256f
Leon Birch <leonbirch333@gmail.com>
2024-04-30 20:27:47 +0100

    created 404 array for unknow url input, turned routing into an array

--- index.php
+++ index.php
@@ -5,12 +5,18 @@ require 'functions.php';
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 
-if ($uri == '/') {
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php'
+];
 
-    require 'controllers/index.php';
-} else if ($uri == '/about') {
-    require 'controllers/about.php';
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+} else {
+    http_response_code(404);
 
-} else if ($uri == '/contact') {
-    require 'controllers/contact.php';
+    require 'views/404.php';
+
+    die();
 }
