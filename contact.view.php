5ff3cf4ff7ef7f9ac9a97ef458c1170b64de14eb
Leon Birch <leonbirch333@gmail.com>
2024-04-30 17:42:51 +0100

    created home, about and contact views and links in nav bar

--- contact.view.php
+++ contact.view.php
@@ -119,12 +119,12 @@
 
     <header class="bg-white shadow">
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact Us</h1>
         </div>
     </header>
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <h1>Hello, welcome to the home page</h1>
+            <h1>Contact us now!</h1>
         </div>
     </main>
 </div>
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
 require 'index.vew.php';
+
+
