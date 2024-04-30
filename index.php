d70415078fd7de9edd9451345cbefee344a54579
Leon Birch <leonbirch333@gmail.com>
2024-04-30 20:36:26 +0100

    abstracted router

--- index.php
+++ index.php
@@ -2,21 +2,4 @@
 
 require 'functions.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php'
-];
-
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    http_response_code(404);
-
-    require 'views/404.php';
-
-    die();
-}
+require 'router.php';
