a98296fe799b1eb621174661d4b5d4977a25b0f5
Leon Birch <leonbirch333@gmail.com>
2024-05-01 14:25:43 +0100

    created new databse and PDO

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
+//require 'router.php';
 
-require 'router.php';
+
+
+
+$db = new Database();
+
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
+
+
+
+
