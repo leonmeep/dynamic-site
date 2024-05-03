b27f3dbf3cc16fdaa5fb6c6a158520d8dc7d56af
Leon Birch <leonbirch333@gmail.com>
2024-05-03 15:01:14 +0100

    added 404 and 403 status codes and checks authorisation of user to view the note

--- controllers/note.php
+++ controllers/note.php
@@ -6,10 +6,21 @@ $db = new Database($config['database']);
 
 $heading = 'Note';
 $title = 'Note';
+$currentUserId = 1;
 
 
-$note = $db->query('SELECT * FROM notes WHERE id = :id',['id' => $_GET['id']])->fetch();
+$note = $db->query('SELECT * FROM notes WHERE  id = :id', [
+    'id' => $_GET['id']
+])->fetch();
 
+if (!$note) {
+    abort();
+}
+
+
+if ($note['user_id'] != $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 
 require 'views/note.view.php';
