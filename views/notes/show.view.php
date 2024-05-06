217fbd1d3a23f8563e1ad099010ef08b31823462
Leon Birch <leonbirch333@gmail.com>
2024-05-06 21:33:31 +0100

    fixed undefined  error on show note page

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -1,29 +1,16 @@
 
-<?php require('views/partials/head.php'); ?>
-<?php require('views/partials/nav.php'); ?>
-<?php require('views/partials/banner.php'); ?>
-
+<?php require('views/partials/head.php') ?>
+<?php require('views/partials/nav.php') ?>
+<?php require('views/partials/banner.php') ?>
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-
-        <ul>
-
-            <?php foreach ($notes as $note) : ?>
-
-                <li>
-                    <a href="/note?id=<?= $note['id'] ?>" class="text-blue-800 hover:underline">
-                        <?= htmlspecialchars($note['body']) ?>
-                    </a>
-                </li>
-
-
-            <?php endforeach; ?>
-        </ul>
-        <p class="mt-6">
-            <a href="/notes/create" class="text-blue-800 hover:underline">Create a new note</a>
+        <p class="mb-6">
+            <a href="/notes" class="text-blue-500 underline">go back...</a>
         </p>
+
+        <p><?= htmlspecialchars($note['body']) ?></p>
     </div>
 </main>
 
- <?php require('views/partials/foot.php'); ?>
+<?php require('views/partials/foot.php') ?>
