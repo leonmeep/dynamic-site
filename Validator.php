2e5a53a75c5f9ab75389695c373d5d5a717eb85f
Leon Birch <leonbirch333@gmail.com>
2024-05-06 19:57:31 +0100

    removed instatiation of validatior and declared it static

--- Validator.php
+++ Validator.php
@@ -2,7 +2,7 @@
 
 class Validator
 {
-    public function string($value, $min = 1, $max = INF): bool
+    public static function string($value, $min = 1, $max = INF): bool
     {
         $value = trim($value);
         return strlen($value) >= $min && strlen($value) <= $max;
